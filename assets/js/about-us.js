7a880af8e13ce300b79ec8c3fd517bfc6d31fbec
SergAlKoba <Serg.Al.Koba@gmail.com>
2020-11-18 10:21:37 +0200

    about

---
+++ assets/js/about-us.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    if($(window).width() >= 768){
+
+        $('.about-us-item .img').matchHeight({
+            byRow: false
+        }); 
+
+    }
+
+
+});
+
+$(window).resize(function(){
+
+    
+    
+});
